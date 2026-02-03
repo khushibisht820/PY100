2f2d0aa9c8ce1aec8a0b63efd1b6a77928bf4814
Khushi Bisht <khushib123456@gmail.com>
2026-02-03 23:19:33 +0530

    update

---
+++ day5.py
@@ -0,0 +1,21 @@
+x = 9
+print(type(x))
+
+y = 7
+print(type(y))
+
+z = x + y
+print(type(z))
+print("addition :", z)
+
+z = x * y
+print(type(z))
+print("multiplication :", z)
+
+z = x / y
+print(type(z))
+print("division :", z)
+
+z = x - y
+print(type(z))
+print("subtraction :", z)
