21985e5e7c04a62c4590dd93ed53a855843d4b92
Khushi Bisht <khushib123456@gmail.com>
2026-02-05 20:21:51 +0530

    update

---
+++ day10.py
@@ -0,0 +1,9 @@
+seconds = float(input("Enter seconds: "))
+
+hours = seconds // 3600
+minutes = (seconds % 3600) // 60
+remaining_seconds = seconds % 60
+
+print("Hours:", hours)
+print("Minutes:", minutes)
+print("Remaining seconds:", remaining_seconds)
