5eacbcfea9002439c5019cee3fad985f8ed994fa
Khushi Bisht <khushib123456@gmail.com>
2026-02-10 20:14:56 +0530

    update

---
+++ day18.py
@@ -0,0 +1,9 @@
+num = int(input("Enter a number: "))          
+temp = num
+rev = 0
+
+while temp > 0:
+    digit = temp % 10
+    rev = rev * 10 + digit
+    temp = temp // 10
+print("Reversed number:", rev)
