e3db93ae72539f0ce79b636e6f68cdff472732d9
Khushi Bisht <khushib123456@gmail.com>
2026-02-15 19:22:06 +0530

    update

---
+++ day30.py
@@ -0,0 +1,28 @@
+S1 = {"Red", "yellow", "orange", "blue"}         
+S2 = {"violet", "blue", "purple"}
+
+print("Set S1:", S1)
+print("Set S2:", S2)
+
+# Union
+print("Union (S1 | S2):", S1 | S2)
+
+# Intersection
+print("Intersection (S1 & S2):", S1 & S2)
+
+# Difference
+print("S1 - S2:", S1 - S2)
+print("S2 - S1:", S2 - S1)
+
+# Symmetric Difference
+print("Symmetric Difference (S1 ^ S2):", S1 ^ S2)
+
+
+
+
+
+
+
+
+
+
