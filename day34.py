a2285dea4a73a036e6f2ff7d55f88900285f1cde
Khushi Bisht <khushib123456@gmail.com>
2026-02-17 23:07:28 +0530

    update

---
+++ day34.py
@@ -0,0 +1,39 @@
+n = int(input("Enter number of students: "))     
+
+students = {}
+
+for i in range(n):
+    print("\nStudent", i + 1)
+    name = input("Enter name: ")
+    city = input("Enter city: ")
+    students[name] = city
+
+# a) Display all names
+print("\nAll Names:")
+for name in students:
+    print(name)
+
+# b) Display all city names
+print("\nAll Cities:")
+for city in students.values():
+    print(city)
+
+# c) Display student name and city
+print("\nStudent Name and City:")
+for name, city in students.items():
+    print(name, "-", city)
+
+# d) Count number of students in each city
+city_count = {}
+
+for city in students.values():
+    if city in city_count:
+        city_count[city] += 1
+    else:
+        city_count[city] = 1
+
+print("\nNumber of students in each city:")
+for city, count in city_count.items():
+    print(city, ":", count)
+
+
